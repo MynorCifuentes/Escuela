6e23a69838749ab40fd1ff77583388884da26387
Mynor Cifuentes <mynoracifuentes09@gmail.com>
2019-07-13 16:58:40 -0600

    Clase3 completa

--- Program.cs
+++ Program.cs
@@ -7,10 +7,10 @@ namespace Etapa1
     {
         static void Main(string[] args)
         {
-            var escuela = new Escuela("Platzi Academy",2012);
+            var escuela = new Escuela("Platzi Academy",2012,TiposEscuela.Primaria,ciudad:"Bogota");
            escuela.Pais = "Colombia";
            escuela.Ciudad ="Bogota";
-           escuela.TipoEscuela = TiposEscuela.Primaria;
+           
             Console.WriteLine(escuela);
         }
     }
--- entidades/Escuela.cs
+++ entidades/Escuela.cs
@@ -13,11 +13,18 @@ namespace CoreEscuela.entidades
         public string Pais { get; set; }
         public string Ciudad { get; set; }
         public TiposEscuela TipoEscuela { get; set; }
-
-
         public Escuela(string nombre, int año) => (Nombre, AñoDeCreación) = (nombre, año);
-        public override string ToString(){
-           return $"Nombre: {Nombre}, Tipo: {TipoEscuela} \n Pais: {Pais}, Ciudad: {Ciudad}";
+        public Escuela(string nombre, int año, TiposEscuela tipo, string pais ="", string ciudad = "") {
+            (Nombre, AñoDeCreación) = (nombre, año);
+            Ciudad =ciudad;
+        }
+
+
+
+// el simbolo \ dentro de la instruccion nos permite ingresar un caracter especial
+        public override string ToString()
+        {
+            return $"Nombre: \"{Nombre}\", Tipo: {TipoEscuela} \n Pais: {Pais}, Ciudad: {Ciudad}";
 
         }
     }

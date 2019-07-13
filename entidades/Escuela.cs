62d6a721ab4bfbfe4c63d0f79235655979fb2319
Mynor Cifuentes <mynoracifuentes09@gmail.com>
2019-07-13 16:20:35 -0600

    Clase1 Completa

--- entidades/Escuela.cs
+++ entidades/Escuela.cs
@@ -5,20 +5,16 @@ namespace CoreEscuela.entidades
         string nombre;
         public string Nombre
         {
-            get { return nombre; }
+            get { return "Copia "+nombre; }
             set { nombre = value; }
         }
 
         public int AñoDeCreación{get;set;}
         public string Pais { get; set; }
         public string Ciudad { get; set; }
-        private int myVar;
+       
 
-        public Escuela(string nombre, int año){
-            this.nombre=nombre;
-            AñoDeCreación=año;
-
-        }
-      //  public Escuela(string nombre, int año) -> (Nombre,AñoDeCreación) = (nombre,año);
+        
+        public Escuela(string nombre, int año) => (Nombre,AñoDeCreación) = (nombre,año);
     }
 }
